3d9d897380673ac38710a642bf780c243ca041a1
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-04-22 22:14:59 -0300

    feat: add @Email validation to PersonPatchRequest dto

--- src/main/java/com/iagomassucato/crud/template/person/PersonPatchRequest.java
+++ src/main/java/com/iagomassucato/crud/template/person/PersonPatchRequest.java
@@ -1,8 +1,11 @@
 package com.iagomassucato.crud.template.person;
 
+import jakarta.validation.constraints.Email;
+
 public record PersonPatchRequest (
         String firstName,
         String lastName,
+        @Email(message = "invalid email")
         String email,
         String cpf,
         Integer age
